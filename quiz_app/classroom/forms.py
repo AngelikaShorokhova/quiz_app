c1cf311b40833e8a6a06b902fb867b9b17fbafdf
AngelikaShorokhova <63111642+AngelikaShorokhova@users.noreply.github.com>
2023-05-28 20:10:40 +0500

    Initial commit

---
+++ quiz_app/classroom/forms.py
@@ -0,0 +1,121 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.db import transaction
+from django.forms.utils import ValidationError
+from django_select2.forms import Select2MultipleWidget
+
+from classroom.models import (Answer, Question, Student, StudentAnswer,
+                              Subject, User, Team)
+
+
+class TeacherSignUpForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Имя пользователя'
+        self.fields['password1'].label = 'Пароль'
+        self.fields['password2'].label = 'Подтверждение пароля'
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_teacher = True
+        if commit:
+            user.save()
+        return user
+
+
+class StudentSignUpForm(UserCreationForm):
+    interests = forms.ModelMultipleChoiceField(
+        queryset=Subject.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=True
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_student = True
+        user.save()
+        student = Student.objects.create(user=user)
+        student.interests.add(*self.cleaned_data.get('interests'))
+        return user
+
+
+class StudentInterestsForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ('interests',)
+        widgets = {
+            'interests': forms.CheckboxSelectMultiple
+        }
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ('text',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].label = ''
+
+
+class BaseAnswerInlineFormSet(forms.BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+
+        has_one_correct_answer = False
+        for form in self.forms:
+            if not form.cleaned_data.get('DELETE', False):
+                if form.cleaned_data.get('is_correct', False):
+                    has_one_correct_answer = True
+                    break
+        if not has_one_correct_answer:
+            raise ValidationError('Mark at least one answer as correct.', code='no_correct_answer')
+
+
+class TakeQuizForm(forms.ModelForm):
+    answer = forms.ModelChoiceField(
+        queryset=Answer.objects.none(),
+        widget=forms.RadioSelect(),
+        required=True,
+        empty_label=None)
+
+    class Meta:
+        model = StudentAnswer
+        fields = ('answer',)
+
+    def __init__(self, *args, **kwargs):
+        question = kwargs.pop('question')
+        super().__init__(*args, **kwargs)
+        self.fields['answer'].queryset = question.answers.order_by('text')
+        self.fields['answer'].label = 'Варианты ответа: '
+
+
+class TeamForm(forms.ModelForm):
+    class Meta:
+        model = Team
+        fields = ['name', 'quiz', 'students']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].label = 'Название команды'
+        self.fields['quiz'].label = 'Викторина'
+        self.fields['students'].label = 'Студенты'
+
+    students = forms.ModelMultipleChoiceField(
+        queryset=Student.objects.all(),
+        widget=forms.SelectMultiple(attrs={'size': 5}),
+        required=True
+    )
+
+    def clean_students(self):
+        students = self.cleaned_data['students']
+        if len(students) > 5:
+            raise forms.ValidationError('Maximum 5 students allowed in a team.')
+        return students
